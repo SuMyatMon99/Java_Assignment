27544cbdc046a42a3e48f696e6031cf263a12004
SuMyatMon99 <sumyatmon596@gmail.com>
2020-07-22 19:36:11 -0700

    i added Assignment9

---
+++ Assignment9.java
@@ -0,0 +1,36 @@
+package JavaAssignment;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Assignment9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How many numbers you want to enter?");
+		Scanner in = new Scanner(System.in);
+		int userInput = in.nextInt();
+		
+		int [] arr = new int [userInput];
+		int total = 0;
+		
+		for (int i = 0 ; i<arr.length; i++) {
+			System.out.print("Enter number " + (i+1) + ":");
+			arr[i] = in.nextInt();
+		}
+		in.close();
+		
+        for(int i=0; i<arr.length; i++){
+        	total = total + arr[i];
+        }
+
+
+        
+        double avg = total / arr.length;
+        
+        System.out.println("Average value is : " + avg );
+		
+        Arrays.sort(arr);
+        System.out.println("Minimum value is : " + arr[0]);
+        System.out.println("Maximum value is : " + arr[arr.length-1]);
+	}
+
+}
